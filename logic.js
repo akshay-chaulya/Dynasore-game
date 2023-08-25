dbf5e9ccffd62865d1a9ca6d7b7350569d767fce
Akshay Chaulya <akshaychaulya09@gmail.com>
2023-08-26 01:56:49 +0530

    first to start

--- logic.js
+++ logic.js
@@ -1,94 +1,17 @@
-// const musicElem = document.getElementById("music");
-let music = new Audio('music.mp3');
-let gamOverMusic = new Audio('gameover.mp3')
-// musicElem.appendChild(music)
+const worldElem = document.querySelector("[data-world]");
 
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
 
-const runner = document.getElementById("runner");
-const villen = document.getElementById("villen");
-const gameOverMessage = document.getElementById("gamoverMessage")
-const showingSocore = document.querySelector(".showingSocore");
-const confarmToPlay = document.querySelector(".confarmToPlay");
-
-let score = 0;
-let cross = true;
-
-confarmToPlay.addEventListener("click", () => {
-    startGame()
-    music.play()
-    confarmToPlay.style.display = "none"
-})
-
-document.onkeydown = function (e) {
-    // console.log("key code: ", e.keyCode)
-
-    if (e.keyCode == 38) {
-        runner.classList.add('animaRunner');
-
-        setTimeout(() => {
-            runner.classList.remove('animaRunner');
-        }, 1000)
+function setPixelToWorldScale() {
+    let worldToPixelScale
+    if (window.innerWidth / window.innerHeight < WORLD_IDTH / WORLD_HEIGHT) {
+        worldToPixelScale = window.innerWidth / WORLD_WIDTH
     }
-    if (e.keyCode == 39) {
-        let runnerX = parseInt(window.getComputedStyle(runner, null).getPropertyValue('left'));
-        runner.style.left = (runnerX + 150) + "px";
+    else {
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
     }
-    if (e.keyCode == 37) {
-        let runnerY = parseInt(window.getComputedStyle(runner, null).getPropertyValue('left'));
-        runner.style.left = (runnerY - 150) + "px";
-    }
-}
 
-function startGame() {
-    villen.classList.add("villenAni");
-    gameOverSetInterval();
-}
-
-const gameOverSetInterval = () => {
-
-    const gameOverPurpuse = setInterval(() => {
-        let rx = parseInt(window.getComputedStyle(runner, null).getPropertyValue('left'));
-        let ry = parseInt(window.getComputedStyle(runner, null).getPropertyValue('bottom'));
-        let vx = parseInt(window.getComputedStyle(villen, null).getPropertyValue('left'));
-        let vy = parseInt(window.getComputedStyle(villen, null).getPropertyValue('bottom'));
-        let offsetX = Math.abs(rx - vx);
-        let offsetY = Math.abs(ry - vy)
-
-        if (offsetX < 100 && offsetY < 52) {
-            villen.classList.remove('villenAni')
-            gameOverMessage.style.display = 'flex'
-            gameOverMessage.innerHTML = "Gameover"
-            gameOverMessage.style.background = "black"
-            gameOverMessage.style.top = "20vh"
-
-            setTimeout(() => {
-                gameOverMessage.style.display = "none"
-                confarmToPlay.style.display = 'block'
-                confarmToPlay.innerHTML = "Play Again"
-            },2000)
-
-            // clearTimeout(gameOverPurpuse)
-
-            music.pause()
-            gamOverMusic.play()
-            setTimeout(() => {
-                gamOverMusic.pause()
-            }, 1000)
-
-        }
-        else if (offsetX < 50 && cross) {
-            score++;
-            showingSocore.innerHTML = `Score ${score}`;
-            cross = false;
-            setTimeout(() => {
-                cross = true;
-            }, 1000)
-
-            setTimeout(() => {
-                const villenAniDur = parseFloat(window.getComputedStyle(villen, null).getPropertyValue('animation-duration'))
-                let newDuration = villenAniDur + .1;
-                villen.style.animationDuration = newDuration + 's';
-            }, 1000)
-        }
-    }, 10)
+    worldElem.computedStyleMap.width = `#{WORLD_WIDTH * worldToPixelScale}px`
+    worldElem.computedStyleMap.height = `#{WORLD_HEIGHT * worldToPixelScale}px`
 }
